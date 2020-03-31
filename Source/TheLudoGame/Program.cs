932988c3bda541a1938a4d7bf3303397cb2d03ab
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-03-31 09:44:05 +0200

    @singelmamma
    Added classes

--- Source/TheLudoGame/Program.cs
+++ Source/TheLudoGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TheLudoGame
 {
@@ -8,5 +9,29 @@ namespace TheLudoGame
         {
             Console.WriteLine("Hello World!");
         }
+    }
+
+    class Game
+    {
+        int GameID { get; set; }
+        bool Finished { get; set; }
+        List<Player> Players { get; set; }
+    }
+
+    class Player
+    {
+        int PlayerID { get; set; }
+        string PlayerName { get; set; }
+        string PlayerColor { get; set; }
+        List<Token> Tokens { get; set; }
+    }
+
+    class Token
+    {
+        int TokenID { get; set; }
+        int GameBoardPosition { get; set; }
+        int EndLinePosition { get; set; }
+        bool AtEndLine { get; set; }
+        bool InGoal { get; set; }
     }
 }
