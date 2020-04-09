15ed1b10b5b9e77fa6459f78eedf5050fbbc5f64
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-04-09 10:42:17 +0200

    @andremorad

--- Source/TheLudoGameXTest/UnitTest1.cs
+++ Source/TheLudoGameXTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using TheLudoGameEngine;
 using Xunit;
 
 namespace TheLudoGameXTest
@@ -6,9 +7,63 @@ namespace TheLudoGameXTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Token_MovmeToken_Forward_False()
         {
+            Token testToken = new Token();
+            testToken.StepsCounter = 2;
+            testToken.CountTokenSteps(testToken, 5);
+            Assert.IsFalse(testToken.StepsCounter == 8);
+        }
 
+        [Fact]
+        public void Token_MovmeToken_Forward_True()
+        {
+            var testToken = new Token();
+            testToken.StepsCounter = 2;
+            testToken.CountTokenSteps(testToken, 5);
+            Assert.IsTrue(testToken.StepsCounter == 7);
+        }
+
+        [Fact]
+        public void Token_MoveToken_BackwardsFromGoal_False()
+        {
+            var testToken = new Token();
+            testToken.StepsCounter = 44;
+            testToken.CountTokenSteps(testToken, 5);
+            Assert.IsFalse(testToken.StepsCounter == 42);
+        }
+
+        [Fact]
+        public void Token_MoveToken_BackwardsFromGoal_True()
+        {
+            var testToken = new Token();
+            testToken.StepsCounter = 44;
+            testToken.CountTokenSteps(testToken, 5);
+            Assert.IsFalse(testToken.StepsCounter == 41);
+        }
+
+        [Fact]
+        public void Game_CheckWinner_Returns_False()
+        {
+            var testGame = new Game();
+            var testWinner = new Player();
+            for (int i = 0; i < 4; i++)
+            {
+                var testToken = new Token();
+                if (i > 2)
+                {
+                    testToken.InGoal = false;
+                    testWinner.Tokens.Add(testToken);
+                }
+                else
+                {
+                    testToken.InGoal = true;
+                    testWinner.Tokens.Add(testToken);
+                }
+            }
+            testGame.Finished = testGame.CheckForWinner(testWinner);
+            Assert.IsTrue(testGame.Finished == true);
         }
     }
+}
 }
